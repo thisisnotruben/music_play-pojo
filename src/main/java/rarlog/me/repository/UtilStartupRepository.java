555cf535f20beefd338495fb23c74bb507d33bbb
Ruben Alvarez Reyes <thisisnotruben@pm.me>
2026-09-17 19:57:37 -0400

    added util startup for 'seedData'

---
+++ src/main/java/rarlog/me/repository/UtilStartupRepository.java
@@ -0,0 +1,7 @@
+package rarlog.me.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rarlog.me.entity.UtilStartup;
+
+public interface UtilStartupRepository extends JpaRepository<UtilStartup, Long> {
+}
